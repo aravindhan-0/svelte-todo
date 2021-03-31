62e95dc0b5d4e49c16de27ee68686c6112793315
Aravindhan <aravindhan2413km@gmail.com>
2021-03-31 13:45:32 +0530

    Demo api and date api

--- api/demo.js
+++ api/demo.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
-    const uri = 'Hello'
-    res.status(200).json({ name: `John Doe ${uri}` })
-  }
+module.exports = (req, res) => {
+  const uri = 'Hello'
+  res.status(200).json({ name: `John Doe ${uri}` })
+};
